d5ff88521e63b41f9ecf96515e25737385a36165
Olena Zaritska <zaritskaos@gmail.com>
2024-09-15 19:39:54 +0300

    create models Task & Tag

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    description = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    class Meta:
+        ordering = ("completed", "-created_at",)
+
+    def __str__(self) -> str:
+        return self.description
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self) -> str:
+        return self.name
