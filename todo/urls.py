0a19d91fce04ead1f735746ced73daeb038c012d
Olena Zaritska <zaritskaos@gmail.com>
2024-09-17 12:51:28 +0300

    add task update url and view

--- todo/urls.py
+++ todo/urls.py
@@ -4,11 +4,13 @@ from django.urls import path
 from todo.views import (
     TaskListView,
     TaskCreateView,
+    TaskUpdateView,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -14,3 +14,9 @@ class TaskCreateView(generic.CreateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
