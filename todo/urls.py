9ffb3e9655fefcf6db089b1376ec857619e8c33f
Olena Zaritska <zaritskaos@gmail.com>
2024-09-17 12:36:09 +0300

    add task create url and view

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 
-from todo.views import TaskListView
+from todo.views import (
+    TaskListView,
+    TaskCreateView,
+)
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list")
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Task
+from todo.forms import TaskForm
 
 
 class TaskListView(generic.ListView):
     model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
