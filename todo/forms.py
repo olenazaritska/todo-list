aa9882101e5063c1c1eaa2af7096d288da3dcc4a
Olena Zaritska <zaritskaos@gmail.com>
2024-09-19 14:41:31 +0300

    add TagForm and tag_form.html

--- todo/forms.py
+++ todo/forms.py
@@ -17,3 +17,9 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = "__all__"
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = "__all__"
